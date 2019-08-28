63897722eda415eb32dcc25ec13b1538901f9f85
Berik <berik9105@gmail.com>
2019-08-28 16:54:55 +0600

    index

---
+++ js/fancybox.js
@@ -0,0 +1,40 @@
+// $(document).ready(function () {
+//     // $(".fancybox").click(function () {
+//         $.fancybox.open([{
+//             href: 'm',
+//             title: 'Диплом KIOSH'
+//         }, {
+//             href: 'img/bpisma/mail1.jpg',
+//             title: 'Универсияда зимняя 2017'
+//         }, {
+//             href: 'img/bpisma/mail2.jpg',
+//             title: 'Лучший товар Казахстана'
+//         },{
+//             href: 'img/bpisma/mail3.jpg',
+//             title: 'Благодарственное письмо от акима г. Алматы'
+//         },
+//         {
+//             href: 'img/bpisma/mail4.jpg',
+//             title: 'Благодарственное письмо от акима Алматинской области'
+//         },
+//         {
+//             href: 'img/bpisma/mail6.jpg',
+//             title: 'Рекомендация от ТОО "Альянс LTD"'
+//         },
+//         {
+//             href: 'img/bpisma/mail7.jpg',
+//             title: 'Благодарственное письмо от West oil'
+//         },
+//         {
+//             href: 'img/bpisma/mail8.jpg',
+//             title: 'Рекомендация от Zhanros'
+//         },
+//         {
+//             href: 'img/bpisma/mail9.jpg',
+//             title: 'Рекомендация от PetroSnabService'
+//         } ], {
+//             padding: 0
+//         });
+//         return false;
+//     });
+// });
---
+++ slick/slick.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+  autoplay:true,
+  draggable: true,
+  arrows: false,
+  dots: true,
+  fade: true,
+  speed: 900,
+  infinite: true,
+  cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+  touchThreshold: 100
+})
